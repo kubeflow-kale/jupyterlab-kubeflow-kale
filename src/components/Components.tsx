fb0161bdde83c17d03b1018ec0e802db3f866408
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2019-10-13 15:14:04 +0100

    Added input select to choose cell type + support for pipeline-parameters cell type

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -141,6 +141,7 @@ interface IMaterialSelect {
     value: any,
     label: string,
     index: number,
+    helperText?: string
 }
 
 export const MaterialSelect: React.FunctionComponent<IMaterialSelect> = (props) => {
@@ -169,6 +170,11 @@ export const MaterialSelect: React.FunctionComponent<IMaterialSelect> = (props) 
                   }
               },
             }}
+            FormHelperTextProps={{
+                classes: {
+                    root: classes.helperLabel
+                }
+            }}
             className={classes.textField}
             id={props.label}
             label={props.label}
@@ -176,6 +182,7 @@ export const MaterialSelect: React.FunctionComponent<IMaterialSelect> = (props) 
             onChange={evt => props.updateValue((evt.target as HTMLInputElement).value, props.index)}
             margin="dense"
             variant="outlined"
+            helperText={ (props.helperText) ? props.helperText : null }
         >
             {props.values.map((option: any) => (
                 <MenuItem key={option.value} value={option.value}>
